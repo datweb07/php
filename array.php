3352e40e51630db88745b48fc71f40eaf131aa5c
datweb07 <dat82770@gmail.com>
2026-02-21 22:56:11 +0700

    style: Improve formatting and add comments in array.php

--- array.php
+++ array.php
@@ -1,6 +1,7 @@
 <?php
 
 // mảng chỉ mục, mảng kết hợp và mảng đa chiều
+// mảng chỉ mục
 $car_old = array('car1', 'car2');   // cách 1
 echo $car_old[0] . '<br>';
 
@@ -15,6 +16,19 @@ foreach ($car_new as $i){
 echo '<br>';
 
 foreach ($car_new as $key => $value){
-    echo $key . ' ';
+    echo $key . ' ' . '<br>';
+}
+
+
+// mảng kết hợp: key sẽ là chuỗi
+$people = [
+    'name' => 'dat, loc',
+    'age' => '12, 13',
+];
+
+echo $people['name'] . '<br>';
+
+foreach ($people as $key => $value){
+    echo $key . ' ' . '<br>';
 }
 ?>
