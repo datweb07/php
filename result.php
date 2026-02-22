a6e07be8176c0cb17dde81cb6d3432b4da589470
datweb07 <dat82770@gmail.com>
2026-02-22 23:25:31 +0700

    feat: Add form validation for user input in result.php

--- result.php
+++ result.php
@@ -18,4 +18,69 @@ if (!empty($_REQUEST)){
     print_r($_REQUEST);
     echo '<pre>';
 }
+
+
+$fullname = "";
+$error_fullname = "";
+
+$email = "";
+$error_email = "";
+
+$password = "";
+$error_password = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    // kiểm tra họ tên
+    if (empty($_POST['fullname'])){
+        $error_fullname = "Tên không được để trống";
+    } else {
+        $fullname = htmlspecialchars(trim($_POST['fullname']));
+    }
+
+    // kiểm tra email
+    if (empty($_POST['email'])){
+        $error_email = "Email không được để trống";
+    }else {
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $error_email = "Email không đúng định dạng";
+        }
+        else{
+            $email = htmlspecialchars(trim($_POST['email']));
+        }
+    }
+
+
+    // kiểm tra password
+    if (empty($_POST['password'])){
+        $error_password = "Mật khẩu không được để trống";
+    }
+    else {
+        if (strlen($_POST['password']) < 6){
+            $error_password = "Mật khẩu phải chứa 6 ký tự";
+        }
+        else {
+            $password = sha1(trim($_POST['password']));
+        }
+    }
+
+    // in ra
+    if ($error_fullname == ""){
+        echo 'Tên hợp lệ: ' . $fullname . '<br>';
+    }else {
+        echo $error_fullname . '<br>';
+    }
+
+    if ($error_email == ""){
+        echo 'Email hợp lệ: ' . $email . '<br>';
+    }else {
+        echo $error_email . '<br>';
+    }
+
+
+    if ($error_password == ""){
+        echo 'Mật khẩu hợp lệ: ' . $password . '<br>';
+    }else {
+        echo $error_password . '<br>';
+    }
+}
 ?>
---
+++ validate.php
@@ -0,0 +1,32 @@
+<?php
+// validate: xử lý dữ liệu nhập vào từ form
+// $_SERVER
+
+
+
+?>
+
+<style>
+    body{
+        margin: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+    }
+
+    form input{
+        margin-bottom: 8px;
+    }
+</style>
+
+<form action="./result.php" method="POST">
+    <input type="text" name="fullname" placeholder="Họ tên">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Mật khẩu">
+    <button type="submit">Đăng ký</button>
+</form>
