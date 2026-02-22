704779002a292424ed218a5be1beff5cd3970026
datweb07 <dat82770@gmail.com>
2026-02-22 10:43:25 +0700

    style: Fix formatting and spacing in array.php

--- array.php
+++ array.php
@@ -10,12 +10,12 @@ $car_new = ['car1', 'car2'];    // cách 2
 echo $car_new[0] . '<br>';
 
 
-foreach ($car_new as $i){
+foreach ($car_new as $i) {
     echo $i . ' ';
 }
 echo '<br>';
 
-foreach ($car_new as $key => $value){
+foreach ($car_new as $key => $value) {
     echo $key . ' ' . '<br>';
 }
 
@@ -28,7 +28,7 @@ $people = [
 
 echo $people['name'] . '<br>';
 
-foreach ($people as $key => $value){
+foreach ($people as $key => $value) {
     echo $key . ' ' . '<br>';
 }
 
@@ -36,10 +36,10 @@ foreach ($people as $key => $value){
 
 // mảng đa chiều
 $brand = [
-    'asus' => ['tuf', 'vivobook', 'zenbook'], 
-    'apple' => ['macbook', 'iphone'], 
+    'asus' => ['tuf', 'vivobook', 'zenbook'],
+    'apple' => ['macbook', 'iphone'],
     'lenovo' => ['loq', 'legion'],
-    ];
+];
 var_dump($brand['asus']);
 echo '<br>';
 
@@ -47,15 +47,15 @@ echo '<br>';
 var_dump($brand['asus'][0]);
 echo '<br>';
 
-foreach ($brand as $key => $value){
+foreach ($brand as $key => $value) {
     var_dump($key);
 }
 echo '<br>';
 
 
-foreach ($brand as $key => $value){
+foreach ($brand as $key => $value) {
     echo "Hãng $key gồm: " . '<br>';
-    foreach ($value as $i){
+    foreach ($value as $i) {
         echo $i . '<br>';
     }
 }
@@ -75,13 +75,13 @@ echo '<br>';
 
 
 
-foreach (array_keys($home) as $key){
+foreach (array_keys($home) as $key) {
     echo $key . ' ';
 }
 echo '<br>';
 
 
-foreach (array_values($home) as $value){
+foreach (array_values($home) as $value) {
     echo $value . ' ';
 }
 echo '<br>';
@@ -90,26 +90,26 @@ echo '<br>';
 $num = [5, 1, 3, 4];
 sort($num);     // tăng dần
 // rsort($num);     // giảm dần
-foreach ($num as $i){
+foreach ($num as $i) {
     echo $i . ' ';
 }
 
 $push = ['hcm', 'hn'];
-foreach ($push as $p){
+foreach ($push as $p) {
     echo $p . ' ';
 }
 echo '<br>';
 
 array_push($push, 'bp');    // push value vào cuối array
 
-foreach ($push as $p){
+foreach ($push as $p) {
     echo $p . ' ';
 }
 echo '<br>';
 
 array_pop($push);       // pop value cuối array ra
 
-foreach ($push as $p){
+foreach ($push as $p) {
     echo $p . ' ';
 }
 echo '<br>';
@@ -117,44 +117,44 @@ echo '<br>';
 
 
 $shift = ['pc', 'laptop'];
-foreach ($shift as $s){
+foreach ($shift as $s) {
     echo $s . ' ';
 }
 echo '<br>';
 
 array_unshift($shift, 'iphone');    // chèn vào phần tử đầu tiên trong mảng
 
-foreach ($shift as $s){
+foreach ($shift as $s) {
     echo $s . ' ';
 }
 echo '<br>';
 
 array_shift($shift);        // lấy đi phần tử đầu tiên
 
-foreach ($shift as $s){
+foreach ($shift as $s) {
     echo $s . ' ';
 }
 echo '<br>';
 
 
 
-$nums = [1,3,4, 5,6];
+$nums = [1, 3, 4, 5, 6];
 
 $res = array_filter($nums, function ($number) {         // lọc theo logic code trong function
     return $number % 2 == 0;
 });
 
-foreach ($res as $r){
+foreach ($res as $r) {
     echo $r . ' ';
 }
 echo '<br>';
 
 
-$uni = [1,1,1,2,3,];
+$uni = [1, 1, 1, 2, 3,];
 
 $unique = array_unique($uni);       // lọc và bỏ đi các phần tử trùng lặp
 
-foreach ($unique as $u){
+foreach ($unique as $u) {
     echo $u . ' ';
 }
 echo '<br>';
@@ -171,7 +171,7 @@ for ($i = 0; $i < count($mer); $i++) {
         echo ' Love ';
     }
 }
-echo '<br>';    
+echo '<br>';
 
 
 $a = 'Toi yeu em';
@@ -183,7 +183,7 @@ $b = explode(' ', $a);      // tách string thành array
 var_dump($b);
 echo '<br>';
 
-foreach ($b as $i){
+foreach ($b as $i) {
     echo $i . ' ';
 }
 echo '<br>';
