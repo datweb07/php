2bd77194f0621eccb453ed7e725f2bdc685bf0b7
datweb07 <dat82770@gmail.com>
2026-02-23 11:42:34 +0700

    feat(session): Add session management functionality

---
+++ session.php
@@ -0,0 +1,17 @@
+<?php
+/*
+session (phiên làm việc): là nơi lưu trữ thông tin trên server, cho mỗi người dùng
+
+Khác với cookie (lưu trữ trên trình duyệt), session an toàn hơn vì người dùng không truy cập trực tiếp được
+*/
+
+session_start();        // bắt buộc có
+
+// $_SESSION['dat'] = 'Học PHP';   // tạo session có tên là dat
+unset($_SESSION['dat']);
+if (isset($_SESSION['dat'])){
+    echo $_SESSION['dat'];
+}
+
+
+?>
