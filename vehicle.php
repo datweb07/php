1c3eb792e836e7932ada9cf697a8df2652c1be00
datweb07 <dat82770@gmail.com>
2026-03-18 16:18:20 +0700

    feat: add PHP OOP inheritance

---
+++ vehicle.php
@@ -0,0 +1,38 @@
+<?php
+
+class vehicle{
+    public $brand;
+    public function index(){
+        echo 'Loading...' . '<br>';
+    }
+    public function setBrand($brand){
+        $this -> brand = $brand;
+    }
+    
+    public function getBrand(){
+        return $this -> brand;
+    }
+}
+
+class Car extends vehicle{
+    public $model;
+
+    public function setModel($model){
+        $this -> model = $model;
+    }
+
+    public function getInfo(){
+        echo 'Hãng xe: ' . $this -> brand . ', Phiên bản: ' . $this -> model;
+    }
+}
+
+
+$myCar = new Car();
+$myCar -> setBrand('Toyota');
+$myCar -> setModel('TY - 1990');
+
+$myCar -> index();
+$myCar -> getInfo();
+
+
+?>
